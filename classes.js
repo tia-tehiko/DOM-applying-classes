db2edf88ca5d8dbd9893e1897d926a1113bd3c46
Tia TeHiko <tiatehiko@gmail.com>
2020-07-07 16:15:15 +1200

    Changed colours of Circles in DOM

--- classes.js
+++ classes.js
@@ -5,8 +5,10 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one()
+  two()
+  three()
+  makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -19,7 +21,21 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
+function two () {
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
 
 // CREATE FUNCTION three HERE
 
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('pink')
+}
+
 // CREATE FUNCTION makeVisible HERE
+
+function makeVisible () {
+  var makeVisible = document.getElementsByClassName('invisible')[0]
+  makeVisible.classList.add('visible')
+}
